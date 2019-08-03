53a44a36379c79ddc2d60cec3f364be8a53b662a
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-03 22:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function increaseRankby(n) {
-  const rankedList = document.querySelectorAll('.ranked-list');
-  
-  for (let i = 0, l = rankedList.length; i < l; i++){
-    let child = rankedList[i].child;
-    
-    for (let j = 0, k = child.length;j < k; j++) {
-      child[j].innerHTML = parseInt(child[j].innerHTML + n);
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
-}
+  }
 }
 
 function deepestChild() {
