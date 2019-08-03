5db7fcadcddf0369a31005c4ab1ad6ea1070c35f
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-03 22:27:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ function increaseRankby(n) {
 }
 }
 
-function deepestChild(){
-  let node = document.getElementByID('grand-node');
+function deepestChild() {
+  let node = document.getElementById('grand-node');
   let nextNode = node.children[0];
-  
+
   while (nextNode) {
     node = nextNode;
     nextNode = node.children[0];
   }
-  
+
   return node;
 }
