1da37972de80e74a53938f7d13ed27bf8e279c08
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-03 22:20:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
+function increaseRankby(n) {
+  const rankedList = document.querySeelctorAll('.ranked-list')
+  
+  for (let i = 0, l = rankedList.length; i < 1; i++){
+    let child = rankedList[i].child
+    
+    for (let j = 0, k = child.length;j < k; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML + n)
+    }
+}
+}
